061963d82bfe60fb08a776ac780df7c1fed3abfc
JosephNdegwa <kariukindegwa98@gmail.com>
2021-06-15 09:35:31 +0300

    add practice js file

---
+++ script.js
@@ -0,0 +1,28 @@
+//string
+let name='Harry Porter';
+
+//Boolean
+let option=true;
+
+//number
+let number=4;
+
+//Array
+let shopping=[]; 
+shopping=['Beans','Maize','Cabbage','Potato','Kales'];
+
+//Object
+let pen={
+    type:'ballpoint',
+    color:'black',
+    cost:15
+};
+
+//Array in object
+let emily={
+    age:17,
+    school:'MKU',
+    subjects:['Math','English','Biology','Chemistry','Physics']
+};
+
+console.log(emily.subjects[3]);
